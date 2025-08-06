968e500cb09697ab23c3078f09733d6ebfa3dd41
AriasFederico <fede.ariass33@gmail.com>
2025-08-06 00:37:10 -0300

    agrega contenido y mapea index en cada componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import './App.css'
+import './App.scss';
 
-function App() {
-  return (
-    <>
-    </>
-  )
-}
+import { Header } from './pages/components/header/Header';
+import { Home } from './pages/home/Home';
+import { Products } from './pages/products/Products';
+import { About } from './pages/about/About';
+import { Footer } from './pages/components/footer/Footer';
 
-export default App
+export const App = () => {
+	return (
+		<div className='App'>
+			<Header />
+			<main>
+				<Home />
+				<Products />
+				<About />
+			</main>
+			<Footer />
+		</div>
+	);
+};
