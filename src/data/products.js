15fcbcbdcb15fc3db721904c7c7ce71c8672de71
AriasFederico <fede.ariass33@gmail.com>
2025-08-15 19:15:59 -0300

    agrega banner al fondo del home

--- src/data/products.js
+++ src/data/products.js
@@ -42,32 +42,4 @@ export const productsCards = [
 		img: productImage,
 		price: '$ 1.520',
 	},
-	{
-		name: 'Lorem ipsum dolor',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt',
-		img: productImage,
-		price: '$ 1.520',
-	},
-	{
-		name: 'Lorem ipsum dolor',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt',
-		img: productImage,
-		price: '$ 1.520',
-	},
-	{
-		name: 'Lorem ipsum dolor',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt',
-		img: productImage,
-		price: '$ 1.520',
-	},
-	{
-		name: 'Lorem ipsum dolor',
-		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt',
-		img: productImage,
-		price: '$ 1.520',
-	},
 ];
